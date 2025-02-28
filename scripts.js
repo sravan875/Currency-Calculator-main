d296a8e0f85f5f28d76ac92908cf6075efba7830
sravan875 <168054096+sravan875@users.noreply.github.com>
2025-02-28 22:59:54 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,239 @@
+const BASE_URL = "https://v6.exchangerate-api.com/v6/d1b99b5b5c60423610301f7e/latest";
+
+const dropdowns = document.querySelectorAll(".select-container select");
+const btn = document.querySelector(".convert-button");
+const fromCurr = document.querySelector(".from-country select");
+const toCurr = document.querySelector(".to-country select");
+const msg = document.querySelector("#exchanged-rate");
+const exchange = document.querySelector("#exchange-symbol");
+
+const populateDropdowns = () => {
+    for (let select of dropdowns) {
+        for (currCode in countryList) {
+            let newOption = document.createElement("option");
+            newOption.innerText = currCode;
+            newOption.value = currCode;
+            if (select.name === "from" && currCode === "USD"){
+                newOption.selected = "selected";
+            }
+            if (select.name === "to" && currCode === "INR"){
+                newOption.selected = "selected";
+            }
+            select.append(newOption);
+        }
+        select.addEventListener("change", (evt) => {
+            updateFlag(evt.target);
+        });
+    }
+};
+
+const updateExchangeRate = async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount-container input");
+    let amountValue = amount.value;
+    if(isNaN(amountValue) || amountValue < 1){
+        amountValue = 1;
+        amount.value = 1;
+    }
+    const URL = `${BASE_URL}/${fromCurr.value}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.conversion_rates[toCurr.value];
+    let finalAmount = amountValue * rate;
+    msg.innerText = `${finalAmount.toFixed(2)} ${toCurr.value}`;
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let flag = element.parentElement.querySelector("img");
+    flag.src = newSrc;
+}
+
+function exchangeFlag(){
+    let fromFlag = document.querySelector("#from-flag");
+    let toFlag = document.querySelector("#to-flag");
+    let temp = toFlag.src;
+    toFlag.src = fromFlag.src;
+    fromFlag.src = temp;
+    
+    let fromValue = fromCurr.value;
+    fromCurr.value = toCurr.value;
+    toCurr.value = fromValue;
+}
+
+exchange.addEventListener("click", exchangeFlag);
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate(evt);
+});
+
+window.addEventListener("load", (evt) => {
+    populateDropdowns();
+    updateExchangeRate(evt);
+});
+
+// Country List
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
